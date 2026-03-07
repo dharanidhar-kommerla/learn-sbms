715bda811614130883e74e0479c1e1182822ddc9
dharanidhar-kommerla <dharanidharkommerla2001@gmail.com>
2026-03-07 20:52:49 +0530

    HAS-A Relation example. Classes are tightly coupled. [Restructuring]

---
+++ 01-1-CarApp-Has-A/src/com/targetclasses/Car.java
@@ -0,0 +1,18 @@
+package com.targetclasses;
+
+import com.dependentclasses.*;
+import java.util.*;
+
+public class Car {
+	Engine en = new Engine();
+	public void drive() {
+		int status = en.start();
+		if(status==1) {
+			System.out.println("Journey Started");
+		} else {
+			System.out.println("Problem in Engine");
+		}
+		System.out.println("HAS-A Relation. Classes are tightly coupled");
+
+	}
+}
