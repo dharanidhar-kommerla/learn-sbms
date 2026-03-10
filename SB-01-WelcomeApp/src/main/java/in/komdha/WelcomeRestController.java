b293e824b9b67008ce061a505de1371915cdb246
dharanidhar-kommerla <dharanidharkommerla2001@gmail.com>
2026-03-10 21:29:41 +0530

    SpringBoot Welcome App

---
+++ SB-01-WelcomeApp/src/main/java/in/komdha/WelcomeRestController.java
@@ -0,0 +1,12 @@
+package in.komdha;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	@GetMapping("/")
+	public String getMsg() {
+		return "Welcome to SpringBoot World";
+	}
+}
