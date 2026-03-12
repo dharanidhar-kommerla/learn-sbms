e856fb7aa15a52ac12d75e68ba55ccf9907b2823
dharanidhar-kommerla <dharanidharkommerla2001@gmail.com>
2026-03-12 21:08:29 +0530

    2 demo spring-boot projects are added.

---
+++ SB-02-DemoApp/src/main/java/in/komdha/Sb02DemoAppApplication.java
@@ -0,0 +1,13 @@
+package in.komdha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb02DemoAppApplication.class, args);
+	}
+
+}
---
+++ SB-03-DemoApp/src/test/java/in/komdha/Sb03DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.komdha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb03DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
